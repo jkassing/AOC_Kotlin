f8b7ae6a729318abf888e287fa5ddff66218b8fb
jkassing <35373370+jkassing@users.noreply.github.com>
2021-12-06 15:42:37 +0100

    implemented day05

---
+++ src/Day05.kt
@@ -0,0 +1,35 @@
+fun main() {
+    fun findVentFields(input: List<String> , diagonalAllowed: Boolean): Int {
+        val fields = mutableMapOf<Pair<Int,Int>, Int>()
+        for(line in input){
+            val vents = line.split("->").map { string -> string.filterNot { it.isWhitespace() }.split(",").map { it.toInt() } }
+            var start = Pair(vents[0][0], vents[0][1])
+            val dest = Pair(vents[1][0], vents[1][1])
+            if(start.first != dest.first && start.second != dest.second && !diagonalAllowed) continue
+            fields[start] = fields.getOrDefault(start, 0) + 1
+            while(start != dest){
+                val stepX = if(start.first - dest.first < 0) 1 else if(start.first - dest.first > 0) -1 else 0
+                val stepY = if(start.second - dest.second < 0) 1 else if(start.second - dest.second > 0) -1 else 0
+                val field = Pair(start.first + stepX, start.second + stepY)
+                fields[field] = fields.getOrDefault(field, 0) + 1
+                start = field
+            }
+        }
+        return fields.filter { it.value > 1 }.size
+    }
+    val test = """
+        0,9 -> 5,9
+        8,0 -> 0,8
+        9,4 -> 3,4
+        2,2 -> 2,1
+        7,0 -> 7,4
+        6,4 -> 2,0
+        0,9 -> 2,9
+        3,4 -> 1,4
+        0,0 -> 8,8
+        5,5 -> 8,2""".trimIndent().split("\n")
+
+    val input = readInput("input_day05")
+    println("PART I: ${ findVentFields(input, false) } scans had higher measurements than the previous scan!")
+    println("PART II: ${ findVentFields(input, true) } windows had higher measurements than the previous window!")
+}
