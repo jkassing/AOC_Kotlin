06fa743648078727e0c3983b22683ea935ae6be4
jkassing <35373370+jkassing@users.noreply.github.com>
2021-12-07 16:13:12 +0100

    calculate cumulative fuel in day07 via gaussian sum

--- src/Day07.kt
+++ src/Day07.kt
@@ -1,15 +1,12 @@
 import kotlin.math.abs
 
 fun main() {
-    fun part1(input: List<Int>): Int? {
-        return (0.rangeTo(input.maxOf { it })).minOfOrNull { pos -> input.sumOf { abs(pos - it) } }
-    }
-    fun part2(input: List<Int>): Int? {
-        return (0.rangeTo(input.maxOf { it })).minOfOrNull { pos -> input.sumOf { (0..abs(pos - it)).sum() } }
-    }
+    fun part1(input: List<Int>, fuel: (Int, Int) -> Int): Int? =
+        (0.rangeTo(input.maxOf { it })).minOfOrNull { pos -> input.sumOf { fuel(pos, it) } }
+
     val test = "16,1,2,0,4,2,7,1,2,14".split(",").map { it.toInt() }
     val input = readInput("input_day07").first().split(",").map { it.toInt() }
-    println("PART I: ${part1(input)} minimum fuel required")
-    println("PART II: ${part2(input)} minimum fuel required")
+    println("PART I: ${part1(input) { x: Int, y: Int -> abs(x - y) }} minimum fuel required")
+    println("PART II: ${part1(input) { x: Int, y: Int -> (abs(x - y) * (abs(x - y)+1)) / 2 }} minimum fuel required")
 }
 
